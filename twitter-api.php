dfd0b58727e4e93a3a9e085e7bba67d19a2d4973
Neil Sweeney <neil@wolfiezero.com>
2014-01-23 11:59:26 +0000

    added github plugin URI

--- twitter-api.php
+++ twitter-api.php
@@ -12,6 +12,8 @@
  * Author URI:  http://wolfiezero.com/
  * License:     GPL-2.0+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ * GitHub Plugin URI: https://github.com/fubralimited/wp-plugin-twitter-api
+ * GitHub Branch:     master
  */
 
 // If this file is called directly, abort.
